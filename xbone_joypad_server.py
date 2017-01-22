841b9428ae50ce0f3d07d3d97441cc57fc48e860
Rob Streeting <robsws@gmail.com>
2017-01-22 16:08:58 +0000

    Adding turning control to server.

--- xbone_joypad_server.py
+++ xbone_joypad_server.py
@@ -2,15 +2,28 @@ import xmlrpclib
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from gopigo import *
 
+throttle_multiplier = 2
+turn_multiplier = 1
+
 def move_robot(turn, forward):
   print(str(forward) + "," + str(turn))
+  # throttle
   if forward < 0:
     fwd()
   elif forward > 0:
     bwd()
   else:
     stop()
-  set_speed(abs(forward))
+  set_speed(abs(forward)*speed_multiplier)
+  # turning
+  if turn < 0:
+    # turn left
+    set_right_speed(abs(forward) + abs(turn) * turn_multiplier)
+    set_left_speed(abs(forward) - abs(turn) * turn_multiplier)
+  elif turn > 0:
+    # turn right
+    set_right_speed(abs(forward) - abs(turn) * turn_multiplier)
+    set_left_speed(abs(forward) + abs(turn) * turn_multiplier)
   return 1
 
 server = SimpleXMLRPCServer(("dex.local", 8000))
