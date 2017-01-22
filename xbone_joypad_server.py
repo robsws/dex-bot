52f6a7669e9c1aad67bc9f3c57fc31648a5dbc9c
Rob Streeting <robsws@gmail.com>
2017-01-22 14:51:13 +0000

    Adding robot forward/backward controls to server.

--- xbone_joypad_server.py
+++ xbone_joypad_server.py
@@ -1,11 +1,18 @@
 from xmlrpc.server import SimpleXMLRPCServer
-# from gopigo import *
+from gopigo import *
 
-def move_robot(x, y):
-  print(str(x) + "," + str(y))
+def move_robot(forward, turn):
+  print(str(forward) + "," + str(turn))
+  if forward > 0:
+    fwd()
+  elif forward < 0:
+    bwd()
+  else:
+    stop()
+  set_speed(forward)
   return 1
 
-server = SimpleXMLRPCServer(("localhost", 8000))
+server = SimpleXMLRPCServer(("http://dex.local", 8000))
 print("Listening on port 8000")
 server.register_function(move_robot, "move_robot")
 server.serve_forever()
